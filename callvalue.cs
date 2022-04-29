b6e4da83dcefea8b259b20799342ea9dc3bacf3a
manishmahirraomm <99090131+manishmahirraomm@users.noreply.github.com>
2022-04-29 16:03:19 +0530

    Create callvalue.cs

---
+++ callvalue.cs
@@ -0,0 +1,19 @@
+# Program to demonstrate call by value in c#.net.
+Using System;
+Namespace CSharp_Shell
+{
+    Public class Program 
+    {
+    	Void get(int x)
+    	{
+    	X=10;
+    	Console.WriteLine(x);
+    	}
+        Public static void Main()
+        {
+	                      Int x=20;
+			Program p=new Program();
+			Console.WriteLine(“before function call:-“+x);
+			p.get(x);
+			Console.WriteLine(“after function call:-“+x);
+        }     }  }  
