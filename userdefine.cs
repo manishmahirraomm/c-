c83d075630e5db9aa44c233ec0a65021bc203ad7
manishmahirraomm <99090131+manishmahirraomm@users.noreply.github.com>
2022-04-29 14:39:20 +0530

    Create userdefine.cs

---
+++ userdefine.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace table
+{
+ class table
+ {
+ int num;
+ table(int a)
+ {
+ Console.WriteLine("table of "+a+"
+is: \n");
+ for (int i = 1; i <= 10; i++)
+ {
+ Console.WriteLine(a*i);
+ }
+ Console.ReadLine();
+ }
+ static void Main(string[] args)
+ {
+ int num;
+ Console.WriteLine("Enter a number to
+print it's table: ");
+ num = int.Parse(Console.ReadLine());
+ table t1 = new table(num);
+ }
+ }
+}
